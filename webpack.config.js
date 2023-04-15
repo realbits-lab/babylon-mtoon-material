962e3fbabf3e5187ced0ae250aee873a3466415d
realbits-lab <dev.realbits@gmail.com>
2023-04-15 21:30:07 +0900

    Add webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const { resolve } = require('path');
 const { merge } = require('webpack-merge');
 
 const baseConfig = {
-    mode: 'production',
+    //* TODO: Handle later.
+    // mode: 'production',
+    mode: 'development',
     entry: resolve(__dirname, 'src', 'index'),
     module: {
         rules: [
@@ -18,46 +21,60 @@ const baseConfig = {
     },
     resolve: {
         modules: [resolve(__dirname, 'node_modules')],
-        extensions: ['.js', '.ts', '.vert', '.frag'],
+        extensions: ['.js', '.ts'],
     },
-    target: 'web',
+    //* TODO: Handle later.
+    // target: 'web',
+    target: ['es2021'],
 };
 
 module.exports = [
+    merge(baseConfig, {
+        output: {
+            library: {
+                type: 'module',
+            },
+            filename: '[name].es6.js',
+            path: path.resolve(__dirname, 'dist'),
+            environment: { module: true },
+        },
+        externalsType: 'module',
+        experiments: {
+            outputModule: true,
+        },
+    }),
     /**
      * to UMD for npm
      */
-    merge(baseConfig, {
-        output: {
-            library: {
-                name: 'babylon-mtoon-material',
-                type: 'umd',
-            },
-            filename: 'index.module.js',
-        },
-        externals: [
-            /^@babylonjs\/core.*$/,
-        ],
-    }),
+    // merge(baseConfig, {
+    //     output: {
+    //         library: {
+    //             name: 'babylon-mtoon-material',
+    //             type: 'umd',
+    //         },
+    //         filename: 'index.module.js',
+    //     },
+    //     externals: [/^@babylonjs\/core.*$/],
+    // }),
     /**
      * to window.MToonMaterial
      */
-    merge(baseConfig, {
-        output: {
-            library: {
-                name: 'MToonMaterial',
-                type: 'window',
-                export: 'MToonMaterial',
-            },
-            filename: 'index.js',
-        },
-        externals: [
-            ({context, request}, callback) => {
-                if (/^@babylonjs\/core.*$/.test(request)) {
-                    return callback(null, `window BABYLON`);
-                }
-                callback();
-            },
-        ],
-    }),
-]
+    // merge(baseConfig, {
+    //     output: {
+    //         library: {
+    //             name: 'MToonMaterial',
+    //             type: 'window',
+    //             export: 'MToonMaterial',
+    //         },
+    //         filename: 'index.js',
+    //     },
+    //     externals: [
+    //         ({ context, request }, callback) => {
+    //             if (/^@babylonjs\/core.*$/.test(request)) {
+    //                 return callback(null, `window BABYLON`);
+    //             }
+    //             callback();
+    //         },
+    //     ],
+    // }),
+];
